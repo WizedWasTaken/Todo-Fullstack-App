2a21935596d8f6229da28aa8779ef095e134e243
WizedWasTaken <jegerfattig1@gmail.com>
2024-03-08 00:16:52 +0100

    Add dependencies and update login page

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,7 +1,64 @@
-export default function Plan() {
+'use client';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleLogin = async (e: { preventDefault: () => void }) => {
+    e.preventDefault(); // Prevent default form submission
+
+    try {
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+
+      if (!response.ok) throw new Error('Login failed');
+
+      const { token } = await response.json();
+      document.cookie = `token=${token}; path=/`;
+      router.push('/protected');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <main className='flex flex-grow items-center justify-center'>
-      <h1>Log Ind</h1>
-    </main>
+    <div>
+      <form onSubmit={handleLogin}>
+        <label>
+          Username:
+          <input
+            type='text'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <button type='submit'>Log In</button>
+      </form>
+    </div>
   );
 }
+
+export default LoginPage;
