3543b52bf1b4603856dc3bf37bd82a2ef02e8d28
WizedWasTaken <141151679+WizedWasTaken@users.noreply.github.com>
2024-03-07 13:50:39 +0100

    Update eslint configuration and add global styling

---
+++ src/components/non-reusable/HeaderComponent.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="p-5 flex flex-column justify-center items-center border-b-2 border-slate-400">
+      <h1>Header</h1>
+    </header>
+  );
+}
