ae8b11a24406ea0417b01cba89cf9ae99e81c6e6
WizedWasTaken <jegerfattig1@gmail.com>
2024-03-10 19:21:21 +0100

    Add Facebook icon to registration page

--- src/app/(pages)/register/page.tsx
+++ src/app/(pages)/register/page.tsx
@@ -7,6 +7,7 @@ import {
   IconBrandGithub,
   IconBrandGoogle,
   IconBrandOnlyfans,
+  IconBrandFacebook,
 } from '@tabler/icons-react';
 
 /**
@@ -33,9 +34,9 @@ export default function RegisterPage() {
       ),
     },
     {
-      name: 'OnlyFans',
+      name: 'Facebook',
       icon: (
-        <IconBrandOnlyfans className='h-4 w-4 text-neutral-800 dark:text-neutral-300' />
+        <IconBrandFacebook className='h-4 w-4 text-neutral-800 dark:text-neutral-300' />
       ),
     },
   ];
@@ -100,7 +101,7 @@ export default function RegisterPage() {
           className='bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]'
           type='submit'
         >
-          Sign up &rarr;
+          Opret Konto &rarr;
           <BottomGradient />
         </button>
 
