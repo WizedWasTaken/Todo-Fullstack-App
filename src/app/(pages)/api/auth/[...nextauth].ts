51e2f338f3e871bcabed2416fc4efb39439e4688
WizedWasTaken <141151679+WizedWasTaken@users.noreply.github.com>
2024-03-11 13:21:04 +0100

    Add Google and Discord authentication providers

--- src/app/(pages)/api/auth/[...nextauth].ts
+++ src/app/(pages)/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import DiscordProvider from 'next-auth/providers/discord';
 
 // TODO: SHIT DOESN'T WORK
 export default NextAuth({
@@ -8,6 +10,14 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_ID as string,
+      clientSecret: process.env.DISCORD_SECRET as string,
     }),
   ],
 });
