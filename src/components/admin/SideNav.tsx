0729de5df08f4f3535001414768aa249a1f6446a
WizedWasTaken <jegerfattig1@gmail.com>
2024-03-25 00:01:12 +0100

    Add scrollbar to SideNav component

--- src/components/admin/SideNav.tsx
+++ src/components/admin/SideNav.tsx
@@ -1,16 +1,17 @@
 import Link from 'next/link';
 import { adminMenuItems } from '@/lib/utils/design/menuItems';
 
+// TODO: If too many items are added, consider adding a scrollbar
 export default function SideNav() {
   return (
-    <aside className='w-52 mr-2 border-r-2 border-slate-400'>
+    <aside className='w-52 mr-2 border-r-2 max-h-fit overflow-y-auto border-slate-400'>
       <nav>
         <ul className='flex flex-col gap-3 py-5'>
           {adminMenuItems.map((link: any) => (
             <li key={link.name}>
               <Link
                 href={link.path}
-                className='flex mx-5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-white text-black bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded-md text-primary-500'
+                className='flex mx-5 text-center dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-white text-black bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded-md text-primary-500'
               >
                 {link.name}
               </Link>
