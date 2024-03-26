bceacc7f474e113a48827a8afb36df445e46abd5
WizedWasTaken <jegerfattig1@gmail.com>
2024-03-26 06:39:02 +0100

    Fix registerUser being submitted when using OAuth buttons

--- src/app/(pages)/register/page.tsx
+++ src/app/(pages)/register/page.tsx
@@ -95,6 +95,7 @@ export default function RegisterPage() {
     },
   ];
 
+  // TODO: registerUser is also submitted when using OAuth buttons. Fix this
   return (
     <main className='flex flex-col flex-grow items-center justify-center w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input'>
       <h2 className='font-bold text-xl text-neutral-800 dark:text-neutral-200 text-center'>
@@ -176,7 +177,7 @@ export default function RegisterPage() {
                 key={index}
                 onClick={() => signIn(option.id)}
                 className='relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]'
-                type='submit'
+                // type='submit'
               >
                 {option.icon}
                 <span className='text-neutral-700 dark:text-neutral-300 text-sm'>
