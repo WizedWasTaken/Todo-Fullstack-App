22cc4f081b9d7e0a0dd5e24888ddeafa5ff80423
WizedWasTaken <141151679+WizedWasTaken@users.noreply.github.com>
2024-04-03 13:01:20 +0200

    Fix formatting and update database connection options

--- src/components/pages/dashboard/SideNav.tsx
+++ src/components/pages/dashboard/SideNav.tsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
 // Imports
-import Link from 'next/link';
-import { dashboardMenuItems } from '@/lib/utils/menuItems'; // Menu items for the admin page
-import { Button } from '@/components/ui-library/button'; // Button component
-import { useState } from 'react';
+import Link from "next/link";
+import { dashboardMenuItems } from "@/lib/utils/menuItems"; // Menu items for the admin page
+import { Button } from "@/components/ui-library/button"; // Button component
+import { useState } from "react";
 
 /*
  * Side navigation for the admin page
@@ -18,37 +18,37 @@ export default function SideNav() {
   return (
     <>
       <div
-        className='h-full flex flex-col gap-2 absolute top-5 right-5 z-50 cursor-pointer md:hidden overflow-hidden'
+        className="h-full flex flex-col gap-2 absolute top-5 right-5 z-50 cursor-pointer overflow-hidden"
         onClick={toggleHamburgerMenu}
       >
         <div
           className={`border-2 border-black dark:border-white w-9 transition-transform duration-300 ${
-            menuOpen ? 'transform -rotate-45 translate-y-4' : ''
+            menuOpen ? "transform -rotate-45 translate-y-4" : ""
           }`}
         ></div>
         <div
           className={`border-2 border-black dark:border-white w-9 transition-transform duration-300 ${
-            menuOpen ? 'translate-x-20' : 'translate-x-0'
+            menuOpen ? "translate-x-20" : "translate-x-0"
           }`}
         ></div>
         <div
           className={`border-2 border-black dark:border-white w-9 transition-transform duration-300 ${
-            menuOpen ? 'transform rotate-45 -translate-y-2' : ''
+            menuOpen ? "transform rotate-45 -translate-y-2" : ""
           }`}
         ></div>
       </div>
       <aside
         className={`border-r-2 overflow-y-auto border-slate-400 transition absolute pt-5 h-full bg-primary/20 left-0 top-0 ${
-          menuOpen ? 'w-[60vw]' : 'w-0 border-r-0'
+          menuOpen ? "w-[60vw]" : "w-0 border-r-0"
         }`}
       >
         <nav>
-          <ul className='flex flex-col gap-3 p-5 h-full'>
+          <ul className="flex flex-col gap-3 p-5 h-full">
             {dashboardMenuItems.map((link: any) => (
               <li key={link.name}>
                 <Button
                   className={`w-full transition ${
-                    menuOpen ? 'translate-x-0' : '-translate-x-[300%]'
+                    menuOpen ? "translate-x-0" : "-translate-x-[300%]"
                   }`}
                   asChild
                 >
--- src/lib/server/dbConnect.ts
+++ src/lib/server/dbConnect.ts
@@ -26,7 +26,7 @@ async function dbConnect() {
 
   if (!cached.promise) {
     const opts = {
-      bufferCommands: false,
+      bufferCommands: true,
     };
 
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
